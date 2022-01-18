c9d0a3a86403590bba87dfa641b57ff9b0caa629
knyazevvb <ks38@mail.ru>
2022-01-18 20:06:02 +0300

    Guess The Number v 0.1
    пока не работают кнопки в методе isPlayerWantsToPlayAgain

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module ru.knyazevvb.guessthenumber.guessthenumber {
+
     requires javafx.controls;
     requires javafx.fxml;
-
 
     opens ru.knyazevvb to javafx.fxml;
     exports ru.knyazevvb;
