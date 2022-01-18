e4a25c8109511e071ce359e789e16b895accaa95
knyazevvb <ks38@mail.ru>
2022-01-18 20:04:40 +0300

    Guess The Number v 0.1
    пока не работают кнопки в методе isPlayerWantsToPlayAgain

--- src/main/java/ru/knyazevvb/GuessTheNumberMain.java
+++ src/main/java/ru/knyazevvb/GuessTheNumberMain.java
@@ -6,10 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.util.Objects;
 
-public class Main extends Application {
+public class GuessTheNumberMain extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("gtn-view.fxml")));
